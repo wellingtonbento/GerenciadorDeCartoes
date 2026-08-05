2c3128b78fbfc6b7335348aced2df128e8071a31
wellington silva <wellingtonbento1042@gmail.com>
2026-08-05 18:02:36 -0300

    Fix flaky tests caused by duplicate entity Ids in seed data

--- tests/CoreTestUtilities/Entities/CardBuilder.cs
+++ tests/CoreTestUtilities/Entities/CardBuilder.cs
@@ -5,10 +5,13 @@ namespace CoreTestUtilities.Entities
 {
     public class CardBuilder
     {
+        private static long _lastId;
         public static Card Build()
         {
+            var id = Interlocked.Increment(ref _lastId);
+
             return new Faker<Card>()
-                .RuleFor(card => card.Id, (f) => f.Random.Long(1, 100))
+                .RuleFor(card => card.Id, (f) => id)
                 .RuleFor(card => card.UserId, (f) => f.Random.Long(1, 100))
                 .RuleFor(card => card.Name, (f) => f.Person.FirstName)
                 .RuleFor(card => card.CreditLimit, (f) => f.Finance.Amount(500, 10000))
--- tests/CoreTestUtilities/Entities/UserBuilder.cs
+++ tests/CoreTestUtilities/Entities/UserBuilder.cs
@@ -6,12 +6,15 @@ namespace CoreTestUtilities.Entities
 {
     public class UserBuilder
     {
+        private static long _lastId;
         public static (User user, string password) Build()
         {
+            var id = Interlocked.Increment(ref _lastId);
+
             var password = new Faker().Internet.Password();
 
             var user = new Faker<User>()
-                .RuleFor(user => user.Id, (f) => f.Random.Long(1, 100))
+                .RuleFor(user => user.Id, (f) => id)
                 .RuleFor(user => user.Name, (f) => f.Person.FirstName)
                 .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
                 .RuleFor(user => user.Password, (f) => PasswordEncripter.EncryptPassword(password));
